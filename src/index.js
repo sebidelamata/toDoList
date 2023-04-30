5a5b47df199b457e7558e55fceb733b40466ba11
Miguel  de la Mata <migueldelamata@Sebis-MacBook-Air.local>
2023-04-30 16:03:54 -0600

    create basic divs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+let docBody = document.querySelector('body');
+
+let headerDiv = document.createElement('div');
+headerDiv.setAttribute('id', 'header');
+
+let bodyDiv = document.createElement('div');
+bodyDiv.setAttribute('id', 'body');
+
+let footerDiv = document.createElement('div');
+footerDiv.setAttribute('id', 'footer');
+
+docBody.appendChild(headerDiv);
+docBody.appendChild(bodyDiv);
+docBody.appendChild(footerDiv);
