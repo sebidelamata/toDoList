d6d84b2f4163ad43b2a30cc6711fa4e19619aa55
Miguel  de la Mata <migueldelamata@Sebis-MacBook-Air.local>
2023-05-06 16:58:11 -0600

    minor adjustments to css

--- src/index.js
+++ src/index.js
@@ -31,16 +31,16 @@ docBody.appendChild(bodyDiv);
 docBody.appendChild(footerDiv);
 
 //sidebar stuff
-let sidebarIconDiv = document.createElement('div');
-sidebarIconDiv.id = 'sidebar-icon-div';
-sidebar.appendChild(sidebarIconDiv);
+let newTaskIconDiv = document.createElement('div');
+newTaskIconDiv.id = 'sidebar-icon-div';
+sidebar.appendChild(newTaskIconDiv);
 let sidebarIconSubDiv1 = document.createElement('div');
 let sidebarIconSubDiv2 = document.createElement('div');
 sidebarIconSubDiv2.id = 'sidebar-icon-sub-div';
 let sidebarIconSubDiv3 = document.createElement('div');
-sidebarIconDiv.appendChild(sidebarIconSubDiv1);
-sidebarIconDiv.appendChild(sidebarIconSubDiv2);
-sidebarIconDiv.appendChild(sidebarIconSubDiv3);
+newTaskIconDiv.appendChild(sidebarIconSubDiv1);
+newTaskIconDiv.appendChild(sidebarIconSubDiv2);
+newTaskIconDiv.appendChild(sidebarIconSubDiv3);
 let sidebarIconSubSubDiv1 = document.createElement('div');
 let sidebarIconSubSubDiv2 = document.createElement('div');
 sidebarIconSubSubDiv2.id = 'sidebar-icon-sub-sub-div';
@@ -50,6 +50,20 @@ let sidebarIconSubSubDiv3 = document.createElement('div');
 sidebarIconSubDiv2.appendChild(sidebarIconSubSubDiv1);
 sidebarIconSubDiv2.appendChild(sidebarIconSubSubDiv2);
 sidebarIconSubDiv2.appendChild(sidebarIconSubSubDiv3);
+
+
+// task list
+let projectsContainer = document.createElement('div');
+projectsContainer.id = 'projects-container';
+projectsContainer.classList.add('button-outline');
+bodyDiv.appendChild(projectsContainer);
+let projectsContainerTitle = document.createElement('div');
+projectsContainerTitle.id = 'projects-container-title';
+projectsContainerTitle.textContent = 'Projects';
+projectsContainer.appendChild(projectsContainerTitle);
+let projectsContainerBody = document.createElement('div');
+projectsContainerBody.id = 'projects-container-body';
+projectsContainer.appendChild(projectsContainerBody);
 
 
 
