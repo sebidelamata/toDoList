fd6f43040b0558b07d5643e9826eadd236285f20
Miguel  de la Mata <sebidelamata@gmail.com>
2023-05-29 17:04:32 -0600

    added ui side to adding new projects

--- src/generalTasks.js
+++ src/generalTasks.js
@@ -238,153 +238,30 @@ editTaskButtons.forEach(function(editTaskButton){
     editTaskButton.addEventListener('click', editTaskFromProject);
 });
 
-// listen to the projects window
-let addProjectButton = document.querySelector('#add-project-button');
 
-const addProjectPopup = () => {
-    
-    let bodyDiv = document.querySelector('body');
-    
-    let greyout = document.createElement('div');
-    greyout.id = 'add-task-greyout';
-    bodyDiv.appendChild(greyout);
-
-    
-    let addProjectContainer = document.createElement('div');
-    addProjectContainer.id = 'add-task-container';
-    addProjectContainer.classList.add('add-task-button-outline');
-    greyout.appendChild(addProjectContainer);
-    let addProjectTitle = document.createElement('div');
-    addProjectTitle.id = 'add-task-title';
-    addProjectTitle.textContent = 'Add Project';
-    addProjectContainer.appendChild(addProjectTitle);
-    let addProjectBody = document.createElement('form');
-    addProjectBody.id = 'add-task-body';
-    addProjectContainer.appendChild(addProjectBody);
-    let projectContainerName = document.createElement('div');
-    projectContainerName.id = 'task-container-name';
-    projectContainerName.classList.add('add-task-inputs');
-    let projectContainerNameLabel = document.createElement('label');
-    projectContainerNameLabel.setAttribute('for', 'name');
-    projectContainerNameLabel.textContent = 'Name';
-    projectContainerName.appendChild(projectContainerNameLabel);
-    let projectContainerNameInput = document.createElement('input');
-    projectContainerNameInput.setAttribute('name', 'name');
-    projectContainerNameInput.setAttribute('required', 'required');
-    projectContainerNameInput.type = 'text';
-    projectContainerName.appendChild(projectContainerNameInput);
-    addProjectBody.appendChild(projectContainerName); 
-    let projectContainerDescription = document.createElement('div');
-    projectContainerDescription.id = 'task-container-description';
-    projectContainerDescription.classList.add('add-task-inputs');
-    let projectContainerDescriptionLabel = document.createElement('label');
-    projectContainerDescriptionLabel.setAttribute('for', 'description');
-    projectContainerDescriptionLabel.textContent = 'Description';
-    projectContainerDescription.appendChild(projectContainerDescriptionLabel);
-    let projectContainerDescriptionInput = document.createElement('input');
-    projectContainerDescriptionInput.setAttribute('name', 'description');
-    projectContainerDescriptionInput.setAttribute('required', 'required');
-    projectContainerDescriptionInput.type = 'text';
-    projectContainerDescription.appendChild(projectContainerDescriptionInput);
-    addProjectBody.appendChild(projectContainerDescription); 
-    let projectContainerDate = document.createElement('div');
-    projectContainerDate.classList.add('add-task-inputs');
-    let projectContainerDateLabel = document.createElement('label');
-    projectContainerDateLabel.setAttribute('for', 'date');
-    projectContainerDateLabel.textContent = 'Due Date';
-    projectContainerDate.appendChild(projectContainerDateLabel);
-    let projectContainerDateInput = document.createElement('input');
-    projectContainerDateInput.setAttribute('name', 'date');
-    projectContainerDateInput.setAttribute('required', 'required');
-    projectContainerDateInput.type = 'date';
-    projectContainerDate.appendChild(projectContainerDateInput);
-    projectContainerDate.id = 'task-container-date';
-    addProjectBody.appendChild(projectContainerDate); 
-
-    //Create array of options to be added
-    var priorityChoices = ["High", "Medium", "Low"];
-
-    let projectContainerPriority = document.createElement('div');
-    projectContainerPriority.id = 'task-container-priority';
-    projectContainerPriority.classList.add('add-task-inputs');
-    let projectContainerPriorityLabel = document.createElement('label');
-    projectContainerPriorityLabel.setAttribute('for', 'priority');
-    projectContainerPriorityLabel.textContent = 'Priority';
-    projectContainerPriority.appendChild(projectContainerPriorityLabel);
-    let projectContainerPriorityInput = document.createElement('select');
-    projectContainerPriorityInput.id = 'task-priority-select';
-    projectContainerPriorityInput.setAttribute('name', 'priority');
-    projectContainerPriorityInput.setAttribute('required', 'required');
-    //Create and append the options
-    for (var i = 0; i < priorityChoices.length; i++) {
-        var option = document.createElement("option");
-        option.value = priorityChoices[i];
-        option.text = priorityChoices[i];
-        projectContainerPriorityInput.appendChild(option);
+// so we can clear the div of child divs
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
-    projectContainerPriority.appendChild(projectContainerPriorityInput);
-    addProjectBody.appendChild(projectContainerPriority);
 
-
-    let submitDiv = document.createElement('div');
-    submitDiv.id = 'submit-div';
-    addProjectBody.appendChild(submitDiv);
-    let submitButton = document.createElement('button');
-    submitDiv.appendChild(submitButton);
-    submitButton.type = 'button';
-    submitButton.textContent = 'Add';
-    submitButton.id = 'submit-button';
-    let cancelDiv = document.createElement('div');
-    cancelDiv.id = 'cancel-div';
-    addProjectBody.appendChild(cancelDiv);
-    let cancelButton = document.createElement('button');
-    cancelButton.type = 'button';
-    cancelDiv.appendChild(cancelButton);
-    cancelButton.textContent = 'Cancel';
-    cancelButton.id = 'cancel-button';
-
-    const addProject = (e) => {
-
-        e.preventDefault();
-        let selectedForm = document.querySelector('form');
-        let nameInput = selectedForm.name.value;
-        let descriptionInput = selectedForm.description.value;
-        let dateInput = selectedForm.date.value;
-        let priorityInput = selectedForm.priority.value;
-
-        
-        ProjectListManager.appendProjectList(
-            nameInput,
-            descriptionInput,
-            dateInput,
-            priorityInput
-        );
-        // remove add task modal
-        greyout.remove();
-        GeneralTask();
-
-    }
-
-    const cancelProject = (e) => {
-        e.preventDefault();
-        // remove add task modal
-        greyout.remove();
-        GeneralTask();
-    }
-
-    let docForm = document.querySelector('form');
-
-    submitButton.addEventListener('click', addProject);
-
-    let cancelSelect = document.querySelector('#cancel-button');
-
-    cancelSelect.addEventListener('click', cancelProject);
-
-};
-
-addProjectButton.addEventListener('click', addProjectPopup);
-
+// display project names on the project list board
+let listedProjects = document.querySelector('#listed-projects'); 
+removeAllChildNodes(listedProjects);
 console.log(ProjectListManager.getProjectList())
+
+
+
+if(ProjectListManager.getProjectList().length > 1){
+    for(let i=1; i<ProjectListManager.getProjectList().length; i++){
+        let projectRow = document.createElement('div');
+        projectRow.id = `project-row-${i}`;
+        listedProjects.appendChild(projectRow);
+        projectRow.textContent = ProjectListManager.getProjectList()[i].getProjectName();
+        projectRow.classList.add('project-row');
+    }
+}
 
 }; 
 
